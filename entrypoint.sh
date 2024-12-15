48ddb045b8f9b96396e3603a7139de51947ac7c1
Mohamed AbdelGawad <muhammadabdelgawwad@gmail.com>
2024-12-15 15:59:20 +0200

    updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,7 @@
 set -e
 
 CERT_DEST="/etc/nginx/certs"
-
-# Acquire certificates for domains
-# Add all required domains here:
+DOMAIN="automagicdeveloper.com"
 DOMAINS="-d automagicdeveloper.com -d www.automagicdeveloper.com -d replicabot.automagicdeveloper.com"
 
 obtain_certificates() {
@@ -12,14 +10,18 @@ obtain_certificates() {
     nginx -s stop || true
     certbot certonly --standalone --preferred-challenges http \
         --non-interactive --agree-tos --email muhammadabdelgawwad@gmail.com \
-        --cert-path ${CERT_DEST}/fullchain.pem --key-path ${CERT_DEST}/privkey.pem \
         $DOMAINS
+    # Copy obtained certificates to /etc/nginx/certs
+    cp /etc/letsencrypt/live/$DOMAIN/fullchain.pem $CERT_DEST/fullchain.pem
+    cp /etc/letsencrypt/live/$DOMAIN/privkey.pem $CERT_DEST/privkey.pem
 }
 
 renew_certificates() {
     echo "Checking and renewing SSL certificates if needed..."
-    certbot renew --quiet \
-        --cert-path ${CERT_DEST}/fullchain.pem --key-path ${CERT_DEST}/privkey.pem
+    certbot renew --quiet
+    # After renewal, ensure certificates are copied
+    cp /etc/letsencrypt/live/$DOMAIN/fullchain.pem $CERT_DEST/fullchain.pem
+    cp /etc/letsencrypt/live/$DOMAIN/privkey.pem $CERT_DEST/privkey.pem
 }
 
 if [ ! -f "${CERT_DEST}/fullchain.pem" ] || [ ! -f "${CERT_DEST}/privkey.pem" ]; then
